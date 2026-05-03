737e55f9bae254bb341fc93d5832d6e377440461
Vaibhav <vaibhavupare757@gmail.com>
2026-05-03 14:53:47 +0530

    v1

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-state-bucket-prod"
-    key            = "prod/ec2/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "tf-locks"
-    encrypt        = true
+    bucket         = "tf-state-bucket-prod-env"
+    key            = "dev/ec2/terraform.tfstate"
+    region         = "us-east-1"
   }
 }
+
--- environments/staging/backend.tf
+++ environments/staging/backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-state-bucket-prod"
-    key            = "staging/ec2/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "tf-locks"
-    encrypt        = true
+    bucket         = "tf-state-bucket-staging-env"
+    key            = "dev/ec2/terraform.tfstate"
+    region         = "us-east-1"
   }
 }
