b95fd4f1d74712067cdcbf57e89b95376826d5fb
Vaibhav <vaibhavupare757@gmail.com>
2026-05-03 12:12:11 +0530

    v2

---
+++ environments/staging/main.tf
@@ -0,0 +1,6 @@
+module "ec2" {
+  source = "../../modules/ec2"
+
+  ami           = "ami-091138d0f0d41ff90" # update per region
+  instance_type = "t2.micro"
+}
--- global/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "global/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    #dynamodb_table = "terraform-locks"
-  }
-}
