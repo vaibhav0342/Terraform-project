2454e7a351987cf46d41b53372ffe2ca34f71886
Vaibhav <vaibhavupare757@gmail.com>
2026-05-03 13:10:40 +0530

    fix terraform provider syntaxs

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend \"s3\" {
-    bucket         = \"tf-state-bucket-prod\"
+    bucket         = \"tf-state-bucket-dev-env\"
     key            = \"dev/ec2/terraform.tfstate\"
     region         = \"ap-south-1\"
-    dynamodb_table = \"tf-locks\"
-    encrypt        = true
   }
 }
