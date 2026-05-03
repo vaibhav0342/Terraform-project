512f3c5521158a5550f9aa0931127ebd9a56f3a5
Vaibhav <vaibhavupare757@gmail.com>
2026-05-03 13:17:13 +0530

    v2

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "tf-state-bucket-dev-env"
     key            = "dev/ec2/terraform.tfstate"
-    region         = "ap-south-1"
+    region         = "us-east-1"
   }
 }
