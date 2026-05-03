62c6544d3ead353e6d65af69987da982064e0449
Vaibhav <vaibhavupare757@gmail.com>
2026-05-03 16:17:07 +0530

    fix: backend key, ami, instance type

--- environments/staging/backend.tf
+++ environments/staging/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-state-bucket-staging-env"
-    key            = "dev/ec2/terraform.tfstate"
+    bucket         = "tf-state-bucket-prod-env"
+    key            = "staging/ec2/terraform.tfstate"
     region         = "us-east-1"
   }
 }
--- environments/staging/terraform.tfvars
+++ environments/staging/terraform.tfvars
@@ -1,2 +1,2 @@
-ami                = "ami-0f5ee92e2d63afc18"   # Amazon Linux 2 (ap-south-1)
+ami                = "ami-0ed094fb1304fd857"   # Amazon Linux 2 
 instance_type      = "t3.micro"
