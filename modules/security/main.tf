61cf2e36d1f8d22b037c7db89d187f1bf0637e0c
Vaibhav <vaibhavupare757@gmail.com>
2026-05-11 00:39:25 +0530

    new one

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -50,3 +50,49 @@ resource "aws_security_group" "alb_sg" {
     Name = "${var.project}-alb-sg"
   }
 }
+
+
+#################################
+# Jenkins Security Group
+#################################
+
+resource "aws_security_group" "jenkins_sg" {
+
+  name        = "${var.project}-jenkins-sg"
+  description = "Jenkins Security Group"
+  vpc_id      = var.vpc_id
+
+  #################################
+  # Jenkins ONLY from ALB
+  #################################
+
+  ingress {
+
+    description = "Jenkins from ALB"
+
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+
+    security_groups = [
+      aws_security_group.alb_sg.id
+    ]
+  }
+
+  #################################
+  # Outbound
+  #################################
+
+  egress {
+
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project}-jenkins-sg"
+  }
+}
