149298b866c57c273d46afba951ba28836218da7
Vaibhav <vaibhavupare757@gmail.com>
2026-05-11 00:27:33 +0530

    v1

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -6,6 +6,7 @@ data "aws_ec2_managed_prefix_list" "cloudfront" {
   name = "com.amazonaws.global.cloudfront.origin-facing"
 }
 
+
 #################################
 # ALB Security Group
 #################################
